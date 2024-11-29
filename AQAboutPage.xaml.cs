95da9ff4611a315619325b396f35cde178a5d6ec
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2024-11-29 07:56:15 -0500

    Agregar archivos de proyecto.

---
+++ AQAboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace AQAppClase;
+
+public partial class AQAboutPage : ContentPage
+{
+	public AQAboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AQAppClase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
