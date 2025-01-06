bae7f7d5b0fb7868ab731e40bb5e902e51967870
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2025-01-05 21:24:27 -0500

    2.0 App MVVC0

---
+++ AQViewModels/AQNotesViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using AQAppClase.AQViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AQAppClase.AQViewModels
+{
+    internal class NotesViewModel : IQueryAttributable
+    {
+        public ObservableCollection<AQViewModels.AQNoteViewModel> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public NotesViewModel()
+        {
+            AllNotes = new ObservableCollection<AQViewModels.AQNoteViewModel>(AQModels.AQNote.LoadAll().Select(n => new AQNoteViewModel(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<AQViewModels.AQNoteViewModel>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(AQViews.AQNotePage));
+        }
+
+        private async Task SelectNoteAsync(AQViewModels.AQNoteViewModel note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(AQViews.AQNotePage)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                AQNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                AQNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                {
+                    matchedNote.Reload();
+                    AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+                }
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Insert(0, new AQNoteViewModel(AQModels.AQNote.Load(noteId)));
+            }
+        }
+    }
+
+}
--- AQViews/AQAboutPage.xaml.cs
+++ AQViews/AQAboutPage.xaml.cs
@@ -2,16 +2,8 @@ namespace AQAppClase.AQViews;
 
 public partial class AQAboutPage : ContentPage
 {
-	public AQAboutPage()
-	{
-		InitializeComponent();
-	}
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    public AQAboutPage()
     {
-        if (BindingContext is AQModels.AQAbout about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
+        InitializeComponent();
     }
 }
